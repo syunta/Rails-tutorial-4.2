8ae7a532c187fb89e1b2773e17f72925d404f780
syunta <kitagawa@timedia.co.jp>
2016-12-19 22:07:44 +0900

    Add ivalid login message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcae)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
