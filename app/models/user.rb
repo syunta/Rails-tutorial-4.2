01c2bf4a51f9e3b0157019c12d815eaf7ec48176
syunta <kitagawa@timedia.co.jp>
2016-12-20 17:56:08 +0900

    Add remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
@@ -15,5 +17,10 @@ class User < ActiveRecord::Base
 
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
